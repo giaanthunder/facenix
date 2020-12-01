352fee16a52d64f870dd190076d384523f043b23
giaanthunder <giaan086.nvktd@gmail.com>
2020-12-02 06:57:18 +0700

    Update clean_all.sh

--- clean_all.sh
+++ clean_all.sh
@@ -2,6 +2,5 @@
 
 rm -rf stgan/output/*
 rm -rf stylegan/output/*
-rm -rf classifier/output/*
-rm -rf boundary_finder/output/stylegan/data/*
-rm -rf boundary_finder/output/stylegan/boundary/*
+rm -rf classifier/models/*
+rm -rf att_vector_finder/stylegan*
